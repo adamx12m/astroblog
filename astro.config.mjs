9a37eeae293f901dc5d849af0e46a0be962630c5
adam murray <adam@kctechworks.com>
2023-06-25 00:27:05 -0500

    url change 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,20 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+// export default defineConfig({
+// 	site: 'https://astroblog-production-a61e.up.railway.app/',
+// 	integrations: [mdx(), sitemap()],
+// });
+
 export default defineConfig({
-	site: 'https://astroblog-production-a61e.up.railway.app/',
-	integrations: [mdx(), sitemap()],
+site: 'https://astroblog-production-a61e.up.railway.app/',
+output: "server",
+//   adapter: node({
+//     mode: "standalone"
+//   }),
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000
+  },
+  integrations: [mdx(), sitemap()],
 });
